af3fcd199a04d290cc99d93108fc8d7ccc2cf5d1
Mehrdad Abdi <m.abdi@aut.ac.ir>
2022-04-11 16:32:04 +0200

    Update installPharo.sh

--- scripts/installPharo.sh
+++ scripts/installPharo.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-curl -s -L https://get.pharo.org/64 >install.sh
+curl -s -L https://get.pharo.org/90 >install.sh
+bash ./install.sh
+rm ./install.sh
+
+curl -s -L https://get.pharo.org/vm90 >install.sh
 bash ./install.sh
 rm ./install.sh
